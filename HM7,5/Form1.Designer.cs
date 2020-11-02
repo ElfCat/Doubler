3e69a9f5d834ea1649eafc5bb0bcaf9cf43e2a15
София Петяева <petyaevas@gmail.com>
2020-11-02 16:50:47 +0300

    Add project files.

---
+++ HM7,5/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace HM7_5
+{
+    partial class Doubler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblGetNum = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(157, 80);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(90, 38);
+            this.btnPlus.TabIndex = 0;
+            this.btnPlus.Text = "+1";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(158, 124);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(90, 36);
+            this.btnDouble.TabIndex = 1;
+            this.btnDouble.Text = "x2";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(158, 166);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(90, 35);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(13, 80);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(14, 15);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "0";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(16, 124);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(96, 15);
+            this.lblText.TabIndex = 4;
+            this.lblText.Text = "Number of actins";
+            this.lblText.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(16, 146);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(14, 15);
+            this.lblCount.TabIndex = 5;
+            this.lblCount.Text = "0";
+            this.lblCount.Click += new System.EventHandler(this.lblCount_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(260, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9F);
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblGetNum
+            // 
+            this.lblGetNum.AutoSize = true;
+            this.lblGetNum.Location = new System.Drawing.Point(5, 26);
+            this.lblGetNum.Name = "lblGetNum";
+            this.lblGetNum.Size = new System.Drawing.Size(10, 15);
+            this.lblGetNum.TabIndex = 9;
+            this.lblGetNum.Text = " ";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(62, 166);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(90, 35);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Go back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Doubler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 212);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lblGetNum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnPlus);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.24F);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Doubler";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Doubler_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblGetNum;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ HM7,5/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HM7_5
+{
+    public partial class Doubler : Form
+    {
+        public int counter = 0;
+        public int userNumber, userCount;
+
+        public Doubler()
+        {
+            InitializeComponent();
+            btnPlus.Text = "+1";
+            btnDouble.Text = "x2";
+            btnReset.Text = "Reset";
+            lblNumber.Text = "0";
+            lblCount.Text = "0";
+            this.Text = "Doubler";
+
+            btnDisable();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!btnBack.Enabled)
+            {
+                btnBack.Enabled = true;
+            }
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            counter++;
+            lblCount.Text = (counter).ToString();
+            Check(int.Parse(lblNumber.Text), counter);
+            remove.Push(lblNumber.Text);
+
+        }
+
+        private void Check(int num, int c)
+        {
+            if (num == userNumber && c == userCount)
+            {
+                lblGetNum.Text = "You did it!!! Good job friend 😀!";
+                btnDisable();
+
+            }
+            else if (c > userCount)
+            {
+                lblGetNum.Text = "You failed 😥\nYour moves biger then given";
+                btnDisable();
+            }
+            else if (userNumber < num)
+            {
+                lblGetNum.Text = "You failed 😥\nYour number biger then given";
+                btnDisable();
+            }
+        }
+
+        public Stack remove = new Stack();
+        
+
+        private void btnDisable()
+        {
+            btnPlus.Enabled = false;
+            btnDouble.Enabled = false;
+            btnReset.Enabled = false;
+            btnBack.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!btnBack.Enabled)
+            {
+                btnBack.Enabled = true;
+            }
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            counter++;
+            lblCount.Text = (counter).ToString();
+            Check(int.Parse(lblNumber.Text), counter);
+            remove.Push(lblNumber.Text);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            lblNumber.Text = "0";
+            lblCount.Text = "0";
+            counter = 0;
+            btnBack.Enabled = false;
+            remove.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCount_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Doubler_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            var rand = new Random();
+            userNumber = rand.Next(1, 100);
+
+            lblNumber.Text = "0";
+            lblCount.Text = "0";
+            counter = 0;
+
+            btnPlus.Enabled = true;
+            btnDouble.Enabled = true;
+            btnReset.Enabled = true;
+            btnBack.Enabled = true;
+
+            userCount = GetStep(userNumber);
+            lblGetNum.Text = "Get number - " + userNumber + " with minimum moves - " + userCount;
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            remove.Pop();
+            if (remove.Count == 0)
+            {
+                lblNumber.Text = "0";
+                lblCount.Text = "0";
+                counter = 0;
+                btnBack.Enabled = false;
+            }
+            else
+            {
+                lblNumber.Text = (string)remove.Peek();
+                counter = counter - 1;
+                lblCount.Text = counter.ToString();
+            }
+        }
+
+        private static int GetStep(int userNumber)
+        {
+            int tempNum = userNumber;
+            int step = 0;
+            while(tempNum != 0)
+            {
+                if (tempNum % 2 == 0)
+                {
+                    tempNum = tempNum / 2;
+                    step++;
+                } else
+                {
+                    tempNum = tempNum - 1;
+                    step++;
+                }
+            }
+
+            return step;
+        }
+    }
+}
